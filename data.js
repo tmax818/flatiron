0adfb0ac60363ba72ac64bbe3736f5543efc93f6
tmax818 <tylermaxwell661@gmail.com>
2020-12-06 19:38:35 -0800

    start data.js

---
+++ data.js
@@ -0,0 +1,4 @@
+export const data = [
+    {id: 0, link: "https://github.com/learn-co-curriculum/welcome-to-learn-verified", lab: false },
+    {id: 1, link: "https://github.com/learn-co-curriculum/prework-intro-navigating-curriculum-on-learn-ide", lab: false },
+]
