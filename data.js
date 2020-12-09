ba16fbe023640e0d97277f141fa4b1e526f28731
tmax818 <tylermaxwell661@gmail.com>
2020-12-08 19:11:16 -0800

    idk

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-export const data = [
+const data = [
     {id: 0, slug: "welcome-to-learn-verified", lab: false },
     {id: 1, slug: "prework-intro-navigating-curriculum-on-learn-ide", lab: false },
     {id: 2, slug: "prework-intro-ask-a-question", lab: false },
@@ -19,4 +19,21 @@ export const data = [
     {id: 17, slug: "hello-world-ruby-v-000", lab: true },
     {id: 18, slug: "irb-readme", lab: false },
     {id: 19, slug: "ruby-lecture-reading-error-messages-v-000", lab: true },
+    {id: 20, slug: "variable-readme", lab: false },
 ]
+
+let anchor = document.createElement('a');
+let div = document.getElementById('root');
+let ul = document.createElement('ul')
+
+let newdata = data.forEach(d => {
+    console.log(d)
+    let a = document.createElement('a')
+    let p = document.createElement('p')
+    a.innerHTML = d.slug
+    a.href = `https://github.com/learn-co-curriculum/${d.slug}`
+    p.appendChild(a)
+    div.appendChild(p)
+    console.log(a)
+})
+    
